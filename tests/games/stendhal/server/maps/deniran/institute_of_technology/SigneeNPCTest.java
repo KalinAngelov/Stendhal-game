ec14e64e390ca0f954d3c76eb9bb13ba12594939
Adrian Szvoren <adrian.szvoren@student.manchester.ac.uk>
2019-11-08 13:54:58 +0000

    Expanded test for SigneeNPC before starting to implement the NPC.

--- tests/games/stendhal/server/maps/deniran/institute_of_technology/SigneeNPCTest.java
+++ tests/games/stendhal/server/maps/deniran/institute_of_technology/SigneeNPCTest.java
@@ -1,5 +1,54 @@
 package games.stendhal.server.maps.deniran.institute_of_technology;
 
-public class SigneeNPCTest {
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+import static utilities.SpeakerNPCTestHelper.getReply;
 
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import games.stendhal.server.entity.npc.SpeakerNPC;
+import games.stendhal.server.entity.npc.fsm.Engine;
+import games.stendhal.server.maps.deniran.institute_of_technology.SigneeNPC;
+import utilities.QuestHelper;
+import utilities.ZonePlayerAndNPCTestImpl;
+
+/**
+ * Test accepting a blue book.
+ *
+ * @author Adrian Szvoren
+ */
+public class SigneeNPCTest extends ZonePlayerAndNPCTestImpl {
+	
+	private static final String ZONE_NAME = "0_deniran_deniran_e";
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+		QuestHelper.setUpBeforeClass();
+
+		setupZone(ZONE_NAME);
+	}
+
+	public SigneeNPCTest() {
+		setNpcNames("Signee");
+		setZoneForPlayer(ZONE_NAME);
+		addZoneConfigurator(new SigneeNPC(), ZONE_NAME);
+	}
+
+	/**
+	 * Tests for hiAndBye.
+	 */
+	@Test
+	public void testHiAndBye() {
+		final SpeakerNPC npc = getNPC("Signee");
+		assertNotNull(npc);
+		final Engine en = npc.getEngine();
+
+		assertTrue(en.step(player, "hello"));
+		assertEquals("Hi. I wish I could learn #Java.", getReply(npc));
+
+		assertTrue(en.step(player, "bye"));
+		assertEquals("Bye, enjoy your day!", getReply(npc));
+	}
 }
