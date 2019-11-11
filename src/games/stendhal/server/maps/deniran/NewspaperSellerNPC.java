160e9e522427077ca24ad61f0d329f1b85cbff26
Jake Fitzwater <jake.fitzwater@student.manchester.ac.uk>
2019-11-11 13:19:07 +0000

    Adds test code to test the dialogue of the NewspaperSellerNPC
    When applied a new test class is added which tests whether the newspaper
    seller is configured correctly and responds to users correctly. A stub 
    newspaper seller class has been added with no implementation, so these
    tests fail

---
+++ src/games/stendhal/server/maps/deniran/NewspaperSellerNPC.java
@@ -0,0 +1,18 @@
+package games.stendhal.server.maps.deniran;
+
+import java.util.Map;
+
+import games.stendhal.server.core.config.ZoneConfigurator;
+import games.stendhal.server.core.engine.StendhalRPZone;
+
+public class NewspaperSellerNPC implements ZoneConfigurator {
+
+
+	@Override
+	public void configureZone(StendhalRPZone zone, Map<String, String> attributes) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+}
