7caba2e68efc30749b0cf15bf1d2e25b2844e9d4
Kalin Angelov <kalin.angelov@student.manchester.ac.uk>
2019-11-12 17:18:50 +0000

    Implementing the newspaper class and implementing the new frame class (when the newspaper is open)
    The newspaper should be open and then a new window should pop.This new
    window is from the NewspaperGui class. Every time the item is onuse this
    new window should be created. The tests are steal failing because
    newspaper is not added in the xml files.

--- src/games/stendhal/server/entity/item/Newspaper.java
+++ src/games/stendhal/server/entity/item/Newspaper.java
@@ -2,28 +2,57 @@ package games.stendhal.server.entity.item;
 
 import java.util.Map;
 
-
+import games.stendhal.server.entity.RPEntity;
 import games.stendhal.server.entity.item.Item;
+import games.stendhal.server.entity.player.Player;
 
-/** 
- * Stub for SleepItem.
- */
 
 
 public class Newspaper extends Item 
 {
-	//private NewspaperGUIPanel guiPanel;
+	private NewspaperGUIPanel guiPanel;
 
 	public Newspaper(final String name, final String clazz, final String subclass, final Map<String,String> attributes) 
 	{
 		super(name, clazz, subclass, attributes);
 	}
 	
-
+	
+	/**
+	 * copy constructor.
+	 *
+	 * @param item
+	 *            item to copy
+	 */
 	public Newspaper(final Newspaper item) 
 	{
 		super(item);
 	}
 	
-	
+	@Override
+	public boolean onUsed(RPEntity user) 
+	{
+		if (user instanceof Player) 
+		{
+			if (isContained())
+			{
+			  guiPanel = new NewspaperGUIPanel();
+			  return true;
+			} // if
+			else 
+			
+				if (!user.nextTo(this))
+				{
+					user.sendPrivateText("The item is too far away to be used.");
+					return false;
+				} // if
+				else
+				{
+					guiPanel = new NewspaperGUIPanel();
+					  return true;
+				}
+		} // if
+		
+		return false;
+	}
 }
